d21f716bea4517f0905313eaf99daea9d00993b8
Hasan Mukhlis <hsnmkls@proton.me>
2024-10-05 10:44:06 +0800

    run forge fmt

--- contracts/src/Charitea.sol
+++ contracts/src/Charitea.sol
@@ -4,49 +4,37 @@ pragma solidity ^0.8.27;
 contract Charitea {
     struct Fund {
         address owner;
-        uint value;
+        uint256 value;
         string data;
     }
 
-    uint fundCount;
+    uint256 fundCount;
     Fund[] public funds;
 
-    event FundCreated(
-        uint indexed fundIndex,
-        address indexed owner,
-        string data
-    );
-    event FundDonated(
-        uint indexed fundIndex,
-        address indexed donator,
-        uint amount
-    );
-    event FundWithdrawn(
-        uint indexed fundIndex,
-        address indexed withdrawer,
-        uint amount
-    );
+    event FundCreated(uint256 indexed fundIndex, address indexed owner, string data);
+    event FundDonated(uint256 indexed fundIndex, address indexed donator, uint256 amount);
+    event FundWithdrawn(uint256 indexed fundIndex, address indexed withdrawer, uint256 amount);
 
     error UnauthorizedWithdrawal();
 
-    function createFund(string calldata data) public returns (uint fundIndex) {
+    function createFund(string calldata data) public returns (uint256 fundIndex) {
         fundIndex = fundCount++;
         funds.push(Fund({owner: msg.sender, value: 0, data: data}));
         emit FundCreated(fundIndex, msg.sender, data);
     }
 
-    function donateFund(uint fundIndex) public payable {
+    function donateFund(uint256 fundIndex) public payable {
         funds[fundIndex].value += msg.value;
 
         emit FundDonated(fundIndex, msg.sender, msg.value);
     }
 
-    function withdrawFund(uint fundIndex) public {
+    function withdrawFund(uint256 fundIndex) public {
         Fund storage fund = funds[fundIndex];
 
         require(msg.sender == fund.owner, UnauthorizedWithdrawal());
 
-        uint toSend = fund.value;
+        uint256 toSend = fund.value;
         fund.value = 0;
 
         payable(fund.owner).transfer(toSend);
--- contracts/test/Charitea.t.sol
+++ contracts/test/Charitea.t.sol
@@ -6,21 +6,9 @@ import {Vm} from "forge-std/Vm.sol";
 import {Charitea} from "../src/Charitea.sol";
 
 contract ChariteaTest is Test {
-    event FundCreated(
-        uint indexed fundIndex,
-        address indexed owner,
-        string data
-    );
-    event FundDonated(
-        uint indexed fundIndex,
-        address indexed donator,
-        uint amount
-    );
-    event FundWithdrawn(
-        uint indexed fundIndex,
-        address indexed withdrawer,
-        uint amount
-    );
+    event FundCreated(uint256 indexed fundIndex, address indexed owner, string data);
+    event FundDonated(uint256 indexed fundIndex, address indexed donator, uint256 amount);
+    event FundWithdrawn(uint256 indexed fundIndex, address indexed withdrawer, uint256 amount);
 
     Charitea charitea;
 
@@ -32,11 +20,9 @@ contract ChariteaTest is Test {
         vm.prank(msg.sender);
         vm.expectEmit(true, true, false, true);
         emit FundCreated(0, msg.sender, "test");
-        uint fundIndex = charitea.createFund("test");
+        uint256 fundIndex = charitea.createFund("test");
 
-        (address owner, uint value, string memory data) = charitea.funds(
-            fundIndex
-        );
+        (address owner, uint256 value, string memory data) = charitea.funds(fundIndex);
 
         assertEq(owner, msg.sender);
         assertEq(value, 0);
@@ -44,21 +30,21 @@ contract ChariteaTest is Test {
     }
 
     function test_DonateFund() public {
-        uint fundIndex = charitea.createFund("test");
+        uint256 fundIndex = charitea.createFund("test");
 
         vm.prank(msg.sender);
         vm.expectEmit(true, true, false, true);
         emit FundDonated(fundIndex, msg.sender, 10);
         charitea.donateFund{value: 10}(fundIndex);
 
-        (, uint value, ) = charitea.funds(fundIndex);
+        (, uint256 value,) = charitea.funds(fundIndex);
         assertEq(value, 10);
     }
 
     function test_WithdrawFund() public {
         Vm.Wallet memory wallet = vm.createWallet("fundOwner");
         vm.prank(wallet.addr);
-        uint fundIndex = charitea.createFund("test");
+        uint256 fundIndex = charitea.createFund("test");
         charitea.donateFund{value: 10}(fundIndex);
 
         vm.expectRevert(Charitea.UnauthorizedWithdrawal.selector);
@@ -68,7 +54,7 @@ contract ChariteaTest is Test {
         vm.expectEmit(true, true, false, true);
         emit FundWithdrawn(fundIndex, wallet.addr, 10);
         charitea.withdrawFund(fundIndex);
-        (, uint value, ) = charitea.funds(fundIndex);
+        (, uint256 value,) = charitea.funds(fundIndex);
         assertEq(value, 0);
         assertEq(wallet.addr.balance, 10);
     }
